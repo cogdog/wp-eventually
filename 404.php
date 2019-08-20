a958a4d448c4be07d6554b93d2b30a48eda37041
CogDog <cogdogblog@gmail.com>
2019-08-20 11:30:47 -0600

    Prep for Reclaim Installer

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+		<h1><a href="<?php echo site_url();?>"><?php bloginfo( 'name' ); ?></a></h1>
+		<h2>Room 404</h2>
+				
+		</header>	
+		
+		<section id="content">
+			
+		<p>Yikes! We could not find what you were looking for.</p>
+
+		<p>Apparently you were looking for something we do not have. No worries, mistakes happen. This site does not have a <em>huge</em> amount of content, so this is most likely a bad link.</p>
+									
+		<p><a href="<?php echo site_url()?>">Return to the front door</a>?</p>
+						
+		</section>	
+
+	  	
+<?php get_footer(); ?>
